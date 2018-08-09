4b97e3dce4e253427e0dd3eccc8feacc2387875a
pangqiqiang <78733149@qq.com>
2018-08-09 16:51:42 +0800

    add another thread

--- convert_mobile_call_report.rb
+++ convert_mobile_call_report.rb
@@ -5,33 +5,50 @@ require './common_funcs'
 require './mongo_handler'
 require './sqlite_treat'
 require './es_handler'
+require 'thread'
 
-file_input = "/home/work/mobile_call_report.json"
 INDEX = "test_mobile_call_report"
 TYPE = "credit_data"
 SQLDB = MyDB.new("ids.db", "id_pairs")
 ES_DB = ELS.new("192.168.30.209:9200", "192.168.30.207:9200", "192.168.30.208:9200")
-BODY_QUEUE = []
+body_queue0=[]; body_queue1=[]; body_queue2=[]; body_queue3=[]; body_queue4=[]; body_queue5=[]
+body_queue6=[]; body_queue7=[];body_queue8=[];body_queue9=[]
+threads = []
 
-File.open(file_input, "r") do |fin|
-	fin.each do |line|
-		output_hash = Hash.new
-		line.chomp!
-		input_hash = JSON.parse(line)
-		output_hash["old_id"] = input_hash["_id"]
-		#忽略身份证号不存在记录
-		next unless output_hash["old_id"].kind_of? String
-		#忽略call记录为空记录
-		next unless input_hash["l_report_contact_list"] and input_hash["l_report_contact_list"].size > 0
-		output_hash["report_id"] = SQLDB.fetch_from_id(output_hash["old_id"])
-		output_hash["report_detail_list"] = input_hash["l_report_contact_list"]
-		output_hash["update_time"] = Time.now.to_i
-#写入es
-		out_body = gen_store_doc_bodies(INDEX, TYPE, output_hash, BODY_QUEUE, 200)
-		ES_DB.bulk_push(out_body) if out_body.is_a? Array
-	end
-	if BODY_QUEUE.size > 0
-		out_body = gen_remain_store_bodies(INDEX, TYPE, BODY_QUEUE)
-		ES_DB.bulk_push(out_body)
+def gen_thr(filename, body_queue)
+	open(filename, 'r') do |fin|
+		fin.each do |line|
+			output_hash = Hash.new
+			line.chomp!
+			input_hash = JSON.parse(line)
+			output_hash["old_id"] = input_hash["_id"]
+			#忽略身份证号不存在记录
+			next unless output_hash["old_id"].kind_of? String
+			#忽略call记录为空记录
+			next unless input_hash["l_report_contact_list"] and input_hash["l_report_contact_list"].size > 0
+			output_hash["report_id"] = SQLDB.fetch_from_id(output_hash["old_id"])
+			output_hash["report_detail_list"] = input_hash["l_report_contact_list"]
+			output_hash["update_time"] = Time.now.to_i
+	#写入es
+			out_body = gen_store_doc_bodies(INDEX, TYPE, output_hash, body_queue, 200)
+			ES_DB.bulk_push(out_body) if out_body.is_a? Array
+		end
+		if body_queue.size > 0
+			out_body = gen_remain_store_bodies(INDEX, TYPE, BODY_QUEUE)
+			ES_DB.bulk_push(out_body)
+		end
 	end
 end
+
+threads << Thread.new { gen_thr("/tmp/mobile_call_report_000", body_queue0)}
+threads << Thread.new { gen_thr("/tmp/mobile_call_report_001", body_queue1)}
+threads << Thread.new { gen_thr("/tmp/mobile_call_report_002", body_queue2)}
+threads << Thread.new { gen_thr("/tmp/mobile_call_report_003", body_queue3)}
+threads << Thread.new { gen_thr("/tmp/mobile_call_report_004", body_queue4)}
+threads << Thread.new { gen_thr("/tmp/mobile_call_report_005", body_queue5)}
+threads << Thread.new { gen_thr("/tmp/mobile_call_report_006", body_queue6)}
+threads << Thread.new { gen_thr("/tmp/mobile_call_report_007", body_queue7)}
+threads << Thread.new { gen_thr("/tmp/mobile_call_report_008", body_queue8)}
+threads << Thread.new { gen_thr("/tmp/mobile_call_report_009", body_queue9)}
+
+threads.map(&:join)
